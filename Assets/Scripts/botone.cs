b3aad5bb1bd3aef9e4b8f0d0e7e85133ab739f3f
lucia2882 <canoperaleslucia@gmail.com>
2026-02-20 15:43:00 +0100

    pa

---
+++ Assets/Scripts/botone.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+public class botone : MonoBehaviour
+{
+    public Button startButton;
+    public Button quitButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        startButton.onClick.AddListener(Empezarjuego);
+        quitButton.onClick.AddListener(Salirjuego);
+    }
+
+    // Update is called once per frame
+public void Empezarjuego()
+{
+    SceneManager.LoadScene("Nivel 1");
+}
+public void Salirjuego()
+{
+    Application.Quit();
+    Debug.Log("Cerrado");
+}
+}
---
+++ Assets/Scripts/cuervo.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class cuervo : MonoBehaviour
+
+{
+    public float velocidad = 2f;
+    public Transform puntoA;
+
+    [Header("Persecución")]
+    public float radioDeteccion = 5f;
+
+    private Transform jugador;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Transform objetivoActual;
+    private bool persiguiendo = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        jugador = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    void Update()
+    {
+        float distanciaJugador = Vector2.Distance(transform.position, jugador.position);
+
+        if (distanciaJugador <= radioDeteccion)
+        {
+            persiguiendo = true;
+        }
+        else
+        {
+            persiguiendo = false;
+        }
+
+        if (persiguiendo)
+        {
+            PerseguirJugador();
+        }
+
+    }
+
+
+    void PerseguirJugador()
+    {
+        transform.position = puntoA.position;
+        animator.SetBool("isWalking", true);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<vida>().PerderVida(1);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radioDeteccion);
+    }
+}
+
